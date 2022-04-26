66758614b490c1969f51083bdf47ff5477e44433
iappa pedroza <itstecnologiacriativa@gmail.com>
2022-04-26 13:10:43 -0300

    Novo Site Usando Next.Js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home () {
     return (
         <div className="container">
             <h1>ITS Tecnologia Criativa</h1>
-            <p>Estudando Laravel</p>
+            <p>Sua chave para excelentes resultados. Sempre!</p>
         </div>
     )
 }
